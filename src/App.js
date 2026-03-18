e9b8ef18251ffef1c830a06acefe82046352ba73
Ian-edu <tsaiyian0801@gmail.com>
2026-03-18 09:30:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EconomicsQuiz = () => {
   const [currentScreen, setCurrentScreen] = useState('menu');
